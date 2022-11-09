b63c715ff607153b22ffd0a09ede589dec4376ee
Kleber M Kardel <molinakardel@gmail.com>
2022-11-09 10:41:38 -0300

    Seleciona elementos do menu mobile no HTML para criar eventos do mesmo

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,4 +1,8 @@
 // Seleção de Elementos
+const menuBtn = document.querySelector("#menu");
+const closeMenuBtn = document.querySelector("#close-menu");
+const menu = document.querySelector("#mobile-navbar");
+
 
 // Funções
 
