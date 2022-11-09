0b826f8f19454aa73c24f35648635a88c40ae66d
Kleber M Kardel <molinakardel@gmail.com>
2022-11-09 10:54:58 -0300

    Seleciona elementos HTML navbar a e mobile-navbar a para criar evento de scroll na página ao clicar nos links

--- JS/scripts.js
+++ JS/scripts.js
@@ -3,6 +3,10 @@ const menuBtn = document.querySelector("#menu");
 const closeMenuBtn = document.querySelector("#close-menu");
 const menu = document.querySelector("#mobile-navbar");
 
+const desktopLinks = document.querySelectorAll("#navbar a");
+const mobileLinks = document.querySelectorAll("#mobile-navbar a");
+
+
 // Funções
 
 // Eventos
