0f956d5cf2556efdc07736db966612e2ee447d52
Kleber M Kardel <molinakardel@gmail.com>
2022-11-09 10:45:19 -0300

    Cria evento de click onde ao clicar no menu mobile o mesmo aparece na tela

--- JS/scripts.js
+++ JS/scripts.js
@@ -3,7 +3,9 @@ const menuBtn = document.querySelector("#menu");
 const closeMenuBtn = document.querySelector("#close-menu");
 const menu = document.querySelector("#mobile-navbar");
 
-
 // Funções
 
 // Eventos
+menuBtn.addEventListener("click", (e) => {
+  menu.classList.add("menu-active");
+})
